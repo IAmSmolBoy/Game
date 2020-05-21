37b5cb91e59074003f6d48c1bf294cad63142c1e
IAmSmlBoy <iamsmolboy>
2020-05-21 16:20:02 +0800

    Planes

---
+++ script.js
@@ -0,0 +1,7 @@
+let plane=document.getElementById("plane")
+let body=document.getElementById('body')
+let div=document.getElementById("div")
+
+document.addEventListener('mousemove',(e)=>{
+    console.log('Hi')
+})
