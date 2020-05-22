0e2b964c6ae768dbe351512b02ea45610d3559d3
IAmSmlBoy <iamsmolboy>
2020-05-22 10:18:04 +0800

    It actually works now

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let plane=document.getElementById("plane")
-let body=document.getElementById('body')
 document.addEventListener('mousemove',(e)=>{
     console.log(e.x+e.y)
     document.head.innerHTML=`<style>#plane{top:${e.y-50}px;
