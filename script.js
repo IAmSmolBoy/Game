fa00594c13bd9dd8ce93885759be9b6a94119526
IAmSmlBoy <iamsmolboy>
2020-05-22 16:08:33 +0800

    Still working on it

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 let plane=document.getElementById("plane")
-document.head.innerHTML=`<style>#plane{top:${e.y-50}px;
-left:${e.x-50}px;
-}</style>`  
-document.addEventListener('mousemove',(e)=>{
-    console.log(e.x+e.y)
-    document.head.innerHTML=`<style>#plane{top:${e.y-50}px;
-    left:${e.x-50}px;
-}</style>`  
-})          
+let body=document.getElementsByTagName("body")
+let startBtn=document.getElementById('start')
+
+function Delete(id){
+    var Thing=document.getElementById(id);
+    Thing.parentNode.removeChild(Thing);
+    return false;
+}
+
+function RanNum(){
+    return `${Math.random()}`.slice(2,4)
+}
+startBtn.addEventListener('click',(e)=>{
+    document.body.innerHTML+=`<div id="plane"></div>`
+    document.addEventListener('keydown',(e)=>{
+        console.log(e.key)
+        if(e.key==='w'||e.key==='ArrowUp'){
+            document.head.innerHTML=`<style>#plane{
+            left:${e.x+1}px;
+            }</style>`
+        }
+        
+    })   
+    Delete('start')
+})
