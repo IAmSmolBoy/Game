293fcc60707465c82de5eb67cffd6805ea41fda2
IAmSmlBoy <iamsmolboy>
2020-05-22 12:27:42 +0800

    stuff

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let plane=document.getElementById("plane")
+document.head.innerHTML=`<style>#plane{top:${e.y-50}px;
+left:${e.x-50}px;
+}</style>`  
 document.addEventListener('mousemove',(e)=>{
     console.log(e.x+e.y)
     document.head.innerHTML=`<style>#plane{top:${e.y-50}px;
