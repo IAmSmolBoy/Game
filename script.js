6368af0a43d9a09cb7c9e65a08e9d9bb2916a589
IAmSmlBoy <iamsmolboy>
2020-05-23 20:50:04 +0800

    Making Pipes

--- script.js
+++ script.js
@@ -3,9 +3,18 @@ var body=document.getElementsByTagName("body")
 var startBtn=document.getElementById('start')
 var window=document.getElementsByTagName('body')
 var head=document.getElementsByTagName('head')
+var pipes=document.getElementsByClassName('pipes')
 var GameOver=true
+var i=0
 var posY=0
 var posX=0
+function Collided(x1,y1,x2,y2){
+    if (x1+50>x2||x1-50<x2||y1+50>y2||y1-50<y2) {
+        return true
+    } else {
+        return false
+    }
+}
 function Delete(id){
     var Thing=document.getElementById(id);
     Thing.parentNode.removeChild(Thing);
@@ -23,8 +32,8 @@ startBtn.addEventListener('click',(e)=>{
     posY=e.y-50
     document.head.innerHTML=`
     <style>#plane{
-        left:${e.x}px;
-        top:${e.y}px;
+        left:${e.x+50}px;
+        top:${e.y+50}px;
         }
         body{
             background-color:skyblue;
@@ -35,42 +44,42 @@ startBtn.addEventListener('click',(e)=>{
     Delete('start')
 })
     document.body.addEventListener('mousemove',(e)=>{
-        if(posY<0){
-            console.log(2)
-            document.body.innerHTML=`<div id="gameover">Game Over</div>
-            <div id="grass"></div>
-            <button id="start">Restart Game</button>
-            <script src="script.js"></script>
-            <link rel="stylesheet" href="style.css">`
-            GameOver=true
-            document.head.innerHTML=`<style>body{
-                background-color:skyblue;
-                width:100vw;
-                height:100vh;}
-                #plane{
-                    top:50vh;
-                }
-                </style>`
-                document.getElementById('start').addEventListener('click',(e)=>{
-                    GameOver=false
-                    document.body.innerHTML+=`<div id="plane"></div>`
-                    posX=e.x-50
-                    posY=e.y-50
-                    document.head.innerHTML=`
-                    <style>#plane{
-                        left:${e.x}px;
-                        top:${e.y}px;
-                        }
-                        body{
-                            background-color:skyblue;
-                            width:100vw;
-                            height:100vh;
-                            cursor:none;}
-                            </style>`
-                    Delete('start')
-                    Delete('gameover')
+        if(posY<0||posX<0||posY>500||posX>1200){
+            if(GameOver===false){
+                document.body.innerHTML=`<div id="gameover">Game Over</div>
+                <div id="grass"></div>
+                <button id="start">Restart Game</button>
+                <script src="script.js"></script>
+                <link rel="stylesheet" href="style.css">`
+                GameOver=true
+                document.head.innerHTML=`<style>body{
+                    background-color:skyblue;
+                    width:100vw;
+                    height:100vh;}
+                    #plane{
+                        top:50vh;
+                    }
+                    </style>`
+                    document.getElementById('start').addEventListener('click',(e)=>{
+                        GameOver=false
+                        document.body.innerHTML+=`<div id="plane"></div>`
+                        posX=e.x+50
+                        posY=e.y+50
+                        document.head.innerHTML=`
+                        <style>#plane{
+                            left:${e.x+50}px;
+                            top:${e.y+50}px;
+                            }
+                            body{
+                                background-color:skyblue;
+                                width:100vw;
+                                height:100vh;
+                                cursor:none;}
+                                </style>`
+                        Delete('start')
+                        Delete('gameover')
                 })
-        }
+        }}
         if(GameOver===false){
             posY+=e.movementY
             posX+=e.movementX
@@ -85,3 +94,9 @@ startBtn.addEventListener('click',(e)=>{
                 cursor:none;}</style>   `
         }
     })
+            // This is the making of the pipes
+function Pipes(height){
+    document.head.innerHTML+=`<style>.pipes{
+        
+    }</style>`
+}
