f330f4ce3e1f57f45bb72548783dc59ef6da2a94
IAmSmlBoy <iamsmolboy>
2020-05-25 20:38:18 +0800

    Pipes Fixed Finally

--- script.js
+++ script.js
@@ -14,24 +14,33 @@ function FindPipeHeight(i){
 }
 function Pipes(height,num){ 
     PipeList.push(`Pipe${num}${height}`)
-    document.body.innerHTML+=`<div id='Pipe${num}${height}${FirstPipeX}' class='pipes'></div>`
-    document.getElementById(`Pipe${num}${height}${FirstPipeX}`).style.left=`5ex`
-    document.getElementById(`Pipe${num}${height}${FirstPipeX}`).style.height=`${height}vh`
+    document.body.innerHTML+=`<div id='Pipe${num}${height}' class='pipes' class='PipePipes'></div>
+    <div id='OtherPipe${num}${height}' class="OtherPipes" class="PipePipes"></div>`
+    document.getElementById(`Pipe${num}${height}`).style.left=`200ex`
+    document.getElementById(`Pipe${num}${height}`).style.height=`${height}vh`
+    document.getElementById(`OtherPipe${num}${height}`).style.height=`${1000-height}vh`
+    document.getElementById(`OtherPipe${num}${height}`).style.left=`200ex`
+    document.getElementById(`OtherPipe${num}${height}`).style.top=`${20+parseInt(height)}vh`
 }
 function Delete(id){
-    var Thing=document.getElementById(id);
-    Thing.parentNode.removeChild(Thing);
+    document.getElementById(id).parentNode.removeChild(document.getElementById(id));
     return false;
 }
 function RanNum(){ 
     var Height=`${Math.random()}`
     if(Height.slice(2,3)===`9`){
-        Height=`8`+`${Height.slice(3,4)}`
+        Height=`7`+`${Height.slice(3,4)}`
     }
     if(Height.slice(2,3)==='0'){
-        Height=`1`+`${Height.slice(3,4)}`
+        Height=`2`+`${Height.slice(3,4)}`
     }
-    if(Height.slice(2,3)!='0'&&Height.slice(2,3)!='9'&&Height.length>3){
+    if(Height.slice(2,3)===`8`){
+        Height=`7`+`${Height.slice(3,4)}`
+    }
+    if(Height.slice(2,3)===`1`){
+        Height=`2`+`${Height.slice(3,4)}`
+       }
+    if(Height.slice(2,3)!='0'&&Height.slice(2,3)!='9'&&Height.length>3&&Height.slice(2,3)!=`1`&&Height.slice(2,3)!=`8`){
         Height=`${Height.slice(2,4)}`
     }
     return Height
@@ -42,29 +51,50 @@ function CreatePipes(){
             var Height=RanNum() 
             Pipes(Height,Count)
             Count++
-            for(i=1;i<=PipeList.length;i++){
-                document.getElementById(PipeList[i-1]).style.left=`${parseInt(document.getElementById(PipeList[i-1]).style.left)-20}vw`
-            }
             CreatePipes()
         }
-    }, 1000);
+    }, 2000);
+}
+function Movement(){
+    setTimeout(function(){
+        if(GameOver===false){
+            for(i=1;i<=PipeList.length;i++){
+                console.log(`Other${PipeList[i-1]}`,document.getElementById(`Other${PipeList[i-1]}`))
+                document.getElementById(`Other${PipeList[i-1]}`).style.left=`${parseInt(document.getElementById(`Other${PipeList[i-1]}`).style.left)-1}ex`
+                document.getElementById(`${PipeList[i-1]}`).style.left=`${parseInt(document.getElementById(`${PipeList[i-1]}`).style.left)-1}ex`
+                if(document.getElementById(`${PipeList[i-1]}`).style.left.slice(0,1)==='-'){
+                    Delete(`${PipeList[i-1]}`)
+                    Delete(`Other${PipeList[i-1]}`)
+                    PipeList.splice(0,1)
+                }
+            }
+            FirstPipeX++
+        }
+        Movement()
+    },100)
 }
 
 startBtn.addEventListener('click',(e)=>{
-    posX=e.x
-    posY=e.y
+    posX=e.x-25
+    posY=e.y-25
     GameOver=false
+    if(GameOver===false){
+        var Height=RanNum() 
+        Pipes(Height,Count)
+        Count++
+    }
     CreatePipes()
+    Movement()
     document.body.innerHTML+=`<div id="plane"></div>`
-    document.getElementById("plane").style.left=`${e.x/7.5}ex`
-    document.getElementById("plane").style.top=`${e.y/7.5}ex`
+    document.getElementById("plane").style.left=`${e.x/7.5+3.5}ex`
+    document.getElementById("plane").style.top=`${e.y/7.5+3.5}ex`
     document.body.style.cursor='none'
     Delete('start')
+    Delete('title')
 })
 document.body.addEventListener('mousemove',(e)=>{
-    if(posX<0||posY<0||posY>500){
+    if(posX<0||posY<0||posY>475){
         if(GameOver===false){
-            PipeList=[]
             document.body.innerHTML=`<div id="gameover">Game Over</div>
             <div id="grass"></div>
             <button id="start">Restart Game</button>
@@ -73,13 +103,20 @@ document.body.addEventListener('mousemove',(e)=>{
             GameOver=true
             document.body.style.cursor='auto'
             document.getElementById('start').addEventListener('click',(e)=>{
+                PipeList=[]
                 posX=100
                 posY=100
                 GameOver=false
+                if(GameOver===false){
+                    var Height=RanNum() 
+                    Pipes(Height,Count)
+                    Count++
+                }
                 CreatePipes()
+                Movement()
                 document.body.innerHTML+=`<div id="plane"></div>`
-                document.getElementById("plane").style.left=`${e.x/7.5}ex`
-                document.getElementById("plane").style.top=`${e.y/7.5}ex`
+                document.getElementById("plane").style.left=`${e.x/7.5+3.5}ex`
+                document.getElementById("plane").style.top=`${e.y/7.5+3.5}ex`
                 document.body.style.cursor=`none`
                 Delete('start')
                 Delete('gameover')
@@ -87,10 +124,10 @@ document.body.addEventListener('mousemove',(e)=>{
         }
     }
     if(GameOver===false){
-        document.getElementById("plane").style.left=`${e.x/7.5}ex`
-        document.getElementById("plane").style.top=`${e.y/7.5}ex`
-        posX=e.x
-        posY=e.y
+        document.getElementById("plane").style.left=`${e.x/7.5+3.5}ex`
+        document.getElementById("plane").style.top=`${e.y/7.5+3.5}ex`
+        posX=e.x-25
+        posY=e.y-25
         document.body.style.cursor=`none` 
     }
 })
